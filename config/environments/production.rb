786a92b4ae4223b7e96d574e9dfa62c2f8dbd744
Brandon Espinoza <espinozab100@gmail.com>
2020-04-10 04:03:56 -0500

    fix for static imgs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@
     # config.assets.css_compressor = :sass
 
     # Do not fallback to assets pipeline if a precompiled asset is missed.
-    config.assets.compile = false
+    config.assets.compile = true
 
     # Enable serving of images, stylesheets, and JavaScripts from an asset server.
     # config.action_controller.asset_host = 'http://assets.example.com'
@@ -85,8 +85,6 @@
         logger.formatter = config.log_formatter
         config.logger    = ActiveSupport::TaggedLogging.new(logger)
     end
-
-    config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
     if ENV["RAILS_LOG_TO_STDOUT"].present?
         logger           = ActiveSupport::Logger.new(STDOUT)
